7709665dbd40451bdb2f7ae94caf143f0a724873
Squarewave34 <132561675+Squarewave34@users.noreply.github.com>
2025-01-31 22:17:13 +0300

    setup

---
+++ Cases/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Cases project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cases.settings')
+
+application = get_wsgi_application()
